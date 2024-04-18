a1964d6271b207aa33ed66fab56adc89f71bf44a
Abdullah-Nazardeen <anazardeenace@gmail.com>
2024-04-18 21:31:59 +0530

    fix window code not being able to deploy

--- src/app/middleware/auth-middleware.tsx
+++ src/app/middleware/auth-middleware.tsx
@@ -1,31 +1,28 @@
-"use client"
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+"use client";
+import { useRouter, usePathname } from "next/navigation";
+import { useEffect } from "react";
 
 const UseAuth = () => {
   const router = useRouter();
-  const { pathname } = window.location;
+  const pathname = usePathname();
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const { pathname } = window.location;
-      const token = localStorage.getItem('twc-token');
-      const isFirstContact = localStorage.getItem("is-first-contact");
+    const token = localStorage.getItem("twc-token");
+    const isFirstContact = localStorage.getItem("is-first-contact");
 
-      if (!token && pathname !== '/login') {
-        router.push('/login');
-      }
-
-      if (token && pathname === '/login') {
-        router.push('/');
-      }
-
-      if (isFirstContact && pathname === '/') {
-        router.push('/contacts');
-      }
+    if (!token && pathname !== "/login") {
+      router.push("/login");
     }
-  }, []);
 
-  return null; 
+    if (token && pathname === "/login") {
+      router.push("/");
+    }
+
+    if (isFirstContact && pathname === "/") {
+      router.push("/contacts");
+    }
+  }, [pathname]);
+
+  return null;
 };
 
 export default UseAuth;
